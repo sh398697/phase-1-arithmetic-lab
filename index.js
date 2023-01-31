87f40e26ad3e63a07b860b01a7918b20c14cef96
Scott <scotthenry1@gmail.com>
2023-01-31 12:52:41 -0500

    Completed lab assignment

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
+//function multiply (num1, num2) {
+//    return (num1 * num2);
+//}
 
-// Write your code here
+let num1 = 1;
+let num2 = 62;
+let multiply = num1 * num2;
+
+let random = Math.floor(Math.random() * 10) + 1;
+
+let num3 = 24;
+let num4 = 5;
+let mod = num3 % num4;
+
+let max = Math.max(1, 2, 3, 20, 5);
